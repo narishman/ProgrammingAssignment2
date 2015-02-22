54e26406dcf22dfdf58bffe7d4f9c6138b7bfbd6
naresh <narishman@gmail.com>
2015-02-22 13:52:09 -0800

    2015_02_22 Programming assignment 2 for R programming class.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These functions help optimize the runtime of creating inverse matrices by caching 
+# the computed inverse matrix and reuse them if the base matrix does not change.
 
-## Write a short comment describing this function
+## Function : makeCacheMatrix
+## This function creates an object with the following attributes
+## x              - holds the matrix
+## invmatrix      - holds the inverse matrix
+## set()          - sets up the matrix within the object for x
+## get()          - gets the matrix from within the object from x
+## setinvmatrix() - sets up the inverse matrix within object for invmatrix
+## getinvmatrix() - gets the inverse matrix within object from invmatrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invmatrix <- NULL
+    set <- function (y) {
+        x <<- y
+        invmatrix <<- NULL
+    }
+    get <- function() x
+    setinvmatrix <- function (i) invmatrix <<- i
+    getinvmatrix <- function () invmatrix
+    list (set = set, get = get,
+          setinvmatrix = setinvmatrix,
+          getinvmatrix = getinvmatrix)
 }
 
 
-## Write a short comment describing this function
+## Function : cacheSolve
+## This function takes in an object of 'makeCacheMatrix' and queries it for an inverse matrix
+## If an inverse matrix is found cached, it returns the inverse matrix
+## Else it computes the inverse matrix and caches it within the object and also returns it to 
+## the caller.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinvmatrix()
+    if (!is.null(i)) {
+        message("getting cached inverted matrix") 
+        return(i)
+    }
+    matrix <- x$get()
+    i <- solve(matrix)
+    x$setinvmatrix(i)
+    i
+    
 }
